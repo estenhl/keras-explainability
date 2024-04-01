53ffe322854a30e33662170e89de57ebaa1600e7
Esten Leonardsen <estenleonardsen@gmail.com>
2024-04-01 18:57:02 +0200

    Working on setup.py file

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name="keras-explainability",
     version="1.0.0",
     author="Esten Høyland Leonardsen",
     author_email="estenleonardsen@gmail.com",
-    packages=["explainability", "explainability.model"],
+    packages=find_packages(),
     url="https://github.com/estenhl/keras-explainability",
     install_requires=[
         "pytest",
